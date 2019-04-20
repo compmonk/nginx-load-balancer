5790c79165e7248eecf590d6e8169a45fce62c33
Zubair Shaikh <zsescape@gmail.com>
2019-04-19 22:44:37 -0400

    Add progress bar

--- visit_server.py
+++ visit_server.py
@@ -6,22 +6,43 @@ def parse_output(text):
   soup = BeautifulSoup(text, 'html.parser')
   return int(soup.title.string[-1])
 
-  
+# Print iterations progress
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█'):
+  """
+  Call in a loop to create terminal progress bar
+  @params:
+    iteration   - Required  : current iteration (Int)
+    total       - Required  : total iterations (Int)
+    prefix      - Optional  : prefix string (Str)
+    suffix      - Optional  : suffix string (Str)
+    decimals    - Optional  : positive number of decimals in percent complete (Int)
+    length      - Optional  : character length of bar (Int)
+    fill        - Optional  : bar fill character (Str)
+  """
+  percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+  filledLength = int(length * iteration // total)
+  bar = fill * filledLength + '-' * (length - filledLength)
+  print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r')
+  # Print New Line on Complete
+  if iteration == total: 
+    print()  
 
-def main(server_address, visits = 2000, servers = 4):
+def main(server_address, visits = 100, slaves = 4):
   loads = {}
-  for server_no in range(1, servers + 1):
-    loads[server_no] = 0
+  for slaves_no in range(1, slaves + 1):
+    loads[slaves_no] = 0
   
 
   with requests.Session() as session:
+    print("Making {0} visits for server address {1} with {2} slaves".format(visits, server_address, slaves))
     for _ in range(visits):
       response = session.get(server_address)
-      server_no = parse_output(response.text)
-      loads[server_no] += 1
+      slaves_no = parse_output(response.text)
+      loads[slaves_no] += 1
+      printProgressBar(_ + 1, visits, prefix = 'Progress:', suffix = 'Complete', length = 50)
   
 
-  for i in range(1, servers + 1):
+  for i in range(1, slaves + 1):
     print("Server {0} visits: {1}".format(i, loads[i]))
   
   print("Total visits", sum(loads.values()))
